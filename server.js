4f61a824f4d623fb7becc6993bcedcc8feb76ab0
David Cutting <dcutting@purplepixie.org>
2019-10-30 18:28:29 +0000

    initial implementation

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+var sub = require('./subtract');
+
+const app = express();
+app.get('/', (req,res) => {
+
+    var output = {
+        'error': false,
+        'string': '',
+        'answer': 0
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    var x = req.query.x;
+    var y = req.query.y;
+    var answer = sub.subtract(x,y);
+
+    output.string = x + '-' + y + '=' + answer;
+    output.answer = answer;
+
+    res.end(JSON.stringify(output));
+});
+
+app.listen(PORT, HOST);
