e09b7be71cf454ae8c12fdd5e3332cfdcfbd5ebb
EDEL <edelellenmurray@hotmail.com>
2021-03-27 15:28:15 +0000

    further testing, robust error handling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 const PORT = 80;
-const HOST = '0.0.0.0';
+const HOST = '127.0.0.1';
 
 var sub = require('./subtract');
 
@@ -21,12 +21,30 @@ app.get('/', (req,res) => {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
 
+    if(!req.query){
+        output.error = true;
+        output.string = 'No arguments recieved - Bad Request'
+        res.status(400).end(JSON.stringify(output));
+        return;
+    }
+
     if(!req.query.x || !req.query.y){
         output.error = true;
+        output.string = 'Invalid parameters - Bad Request'
+        res.status(400).end(JSON.stringify(output));
+        return;
     }
     else {
-        x = req.query.x;
-        y = req.query.y;
+        if(parseInt(req.query.x)&&parseInt(req.query.y)){
+            x = req.query.x;
+            y = req.query.y;
+        }
+        else{
+            output.error = true;
+            output.string = 'Invalid parameters for calculation: params are not valid integers';
+            res.status(400).end(JSON.stringify(output));
+            return;
+        }
     }
 
     var answer = sub.subtract(x,y);
@@ -35,10 +53,14 @@ app.get('/', (req,res) => {
     output.answer = answer;
     if (output.error==false) {
         res.status(200).end(JSON.stringify(output));
+        return;
     } else {
         res.status(400).end(JSON.stringify(output));
+        return;
     }
     
 });
 
 app.listen(PORT, HOST);
+
+module.exports = app; // for testing
